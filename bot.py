5d558632c75f6b0dead7a2dce5d90e502a8a3a74
Dmitry Kurachev <dmkurachev@gmail.com>
2024-01-15 09:50:22 +0500

    removed outdated code, fixed imports and formatted a file

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
-import cfg
-import discord
-import datetime as dt
-import re
-from discord import app_commands
-from discord import Interaction
-from discord.app_commands.errors import MissingAnyRole
-from discord.app_commands.checks import has_any_role
 import aiohttp
+import discord
+from discord import Interaction, app_commands
+from discord.app_commands.checks import has_any_role
+from discord.app_commands.errors import MissingAnyRole
 
+import cfg
 import db
 
 TEST_GUILDS_ID = cfg.TEST_GUILDS_ID
 TEST_GUILDS = [discord.Object(gid) for gid in TEST_GUILDS_ID]
 print([guild.created_at for guild in TEST_GUILDS])
+
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
@@ -30,13 +29,6 @@ client = MyClient(intents=intents)
 tree = app_commands.CommandTree(client)
 
 
-# @tree.command(name='test', \
-#               description='test description', \
-#               guild=discord.Object(id=cfg.guilds['sh']))
-# async def test(interaction, name: str):
-#     await interaction.response.send_message(f'Hi, {name}!')
-
-
 @tree.command(name='vasserman',
               description='vasserman event',)
 async def vasserman(interaction: discord.Interaction,
@@ -48,46 +40,45 @@ async def vasserman(interaction: discord.Interaction,
     if not user:
         try:
             connect = db.DBConnect()
-            select = connect.select(f'SELECT * FROM Vasserman WHERE vsm_points!=0').fetchall()
+            select = connect.select(
+                f'SELECT * FROM Vasserman WHERE vsm_points!=0').fetchall()
             select = sorted(select, key=lambda x: x[2], reverse=True)
             print(select)
             desc = f''
             for res in select:
-                desc += f'<@{res[0]}>: {res[2] if res[2] % 1 != 0 else int(res[2])}\n'
+                desc += f'<@{res[0]}>: {res[2] if res[2] %
+                                        1 != 0 else int(res[2])}\n'
             print(desc)
             await interaction.response.send_message(embed=discord.Embed(title='Вассерман!', description=desc))
         except ConnectionError:
             print('[ERROR] There was an error during db connect!')
-            await interaction.response.send_message \
-                (f'При выполнении команды произошла ошибка!')
-        
+            await interaction.response.send_message(f'При выполнении команды произошла ошибка!')
+
     if not interaction.user.get_role(943843555829510214):
         await interaction.response.send_message('У вас недостаточно прав!')
     try:
         connect = db.DBConnect()
-        select = connect.select(f'SELECT * FROM Vasserman WHERE user_id={user.id}').fetchone()
+        select = connect.select(
+            f'SELECT * FROM Vasserman WHERE user_id={user.id}').fetchone()
         if select:
-            connect.update('Vasserman', f'vsm_points={select[2] + points_delta}', user.id)
+            connect.update('Vasserman', f'vsm_points={
+                           select[2] + points_delta}', user.id)
         else:
             connect.insert('Vasserman', (user.id, 0, points_delta))
     except ConnectionError:
         print('[ERROR] There was an error during db connect!')
-        await interaction.response.send_message \
-            (f'При выполнении команды произошла ошибка!')
-    # except ValueError:
-    #     print('[ERROR] Incorrect values!')
-        await interaction.response.send_message \
-            (f'При выполнении команды произошла ошибка!')
+        await interaction.response.send_message(f'При выполнении команды произошла ошибка!')
+    except ValueError:
+        print('[ERROR] Incorrect values!')
+        await interaction.response.send_message(f'При выполнении команды произошла ошибка!')
     else:
         if points_delta >= 0:
-            await interaction.response.send_message \
-                (embed=discord.Embed(title='Вассерман!',
-                                     description=f'К счёту участника {user.mention}\
+            await interaction.response.send_message(embed=discord.Embed(title='Вассерман!',
+                                                                        description=f'К счёту участника {user.mention}\
                                          было добавлено {points_delta} очков!'))
         else:
-            await interaction.response.send_message \
-                (embed=discord.Embed(title='Вассерман!',
-                                     description=f'Со счёта участника {user.mention}\
+            await interaction.response.send_message(embed=discord.Embed(title='Вассерман!',
+                                                                        description=f'Со счёта участника {user.mention}\
                                          было вычтено {abs(points_delta)} очков!'))
 
 
@@ -109,13 +100,7 @@ async def add_emoji(interaction: Interaction, emoji: str, name: str = None):
         await interaction.response.send_message("Произошла ошибка!")
     else:
         await interaction.response.send_message("Эмодзи <{}:{}:{}> был успешно добавлен!".format(emj_anim, emj_name, emj_id))
-        
-# @vasserman.error
-# async def on_application_command_error(ctx: Interaction, error: discord.DiscordException):
-#     if isinstance(error, MissingAnyRole):
-#         await ctx.edit_original_response("Sorry, only the bot owner can use this command!")
-#     else:
-#         raise error
+
 
 @client.event
 async def on_ready():
@@ -123,12 +108,14 @@ async def on_ready():
     await tree.sync(guild=TEST_GUILDS[1])
     print('Ready!')
 
+
 @client.event
 async def on_message(ctx):
     datetime_format = "%d-%m-%y %H:%M:%S"
     if ctx.guild.id == 542919556155310101:
         with open('msg_log.txt', 'a', encoding='utf-8') as log:
-            log.write(f'[{ctx.created_at.strftime(datetime_format)}] {ctx.author}: {ctx.content}\n')
-        
-        
+            log.write(f'[{ctx.created_at.strftime(datetime_format)}] {
+                      ctx.author}: {ctx.content}\n')
+
+
 client.run(cfg.TOKEN)
